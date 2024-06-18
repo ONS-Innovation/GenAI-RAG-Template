a0ae02438d483f648fd0778bce0fa855860b1aff
Wojciech Przybylski <146734702+WojciechP-ONS@users.noreply.github.com>
2024-06-18 16:34:24 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,13 @@
  * limitations under the License.
  */
 
+output "workload_identity_provider" {
+  value = "${google_iam_workload_identity_pool.github_pool.name}/providers/${google_iam_workload_identity_pool_provider.github.workload_identity_pool_provider_id}"
+}
+
+output "service_account" {
+  value = google_service_account.github_actions.email
+}
 
 output "deployment_ip_address" {
   description = "Web URL link"
