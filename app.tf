706526168b67458647184d3197c5db0e6244600e
Wojciech Przybylski <146734702+WojciechP-ONS@users.noreply.github.com>
2024-06-19 20:09:18 +0100

    Update app.tf

--- app.tf
+++ app.tf
@@ -18,7 +18,6 @@
 resource "google_iam_workload_identity_pool" "gemini-rag" {
   provider                  = google-beta
   project                   = var.project_id
-  workload_identity_pool_id = google_iam_workload_identity_pool.gemini-rag.workload_identity_pool_id
   display_name              = var.pool_display_name
   description               = var.pool_description
   disabled                  = false
