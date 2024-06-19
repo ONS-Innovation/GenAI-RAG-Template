6a47680199f21137e34ec796fe1c9aee09c35a8d
Wojciech Przybylski <146734702+WojciechP-ONS@users.noreply.github.com>
2024-06-19 14:07:33 +0100

    Update app.tf

--- app.tf
+++ app.tf
@@ -1,23 +1,7 @@
-/**
- * Copyright 2024 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 locals {
   project_id   = "hackathon-cp-project-team-1"
   organization = "backstage-dummy-org"
-  repo         = "RAG-Demo-SDP" 
+  repo         = "GenAI-RAG-Template"
 }
 
 resource "google_iam_workload_identity_pool" "github_pool" {
@@ -46,7 +30,6 @@ resource "google_iam_workload_identity_pool_provider" "github" {
   }
 }
 
-
 resource "google_service_account" "github_actions" {
   project      = local.project_id
   account_id   = "github-actions"
@@ -54,7 +37,7 @@ resource "google_service_account" "github_actions" {
 }
 
 resource "google_service_account" "runsa" {
-  project      = module.project-services.project_id
+  project      = local.project_id
   account_id   = "genai-rag-run-sa-${random_id.id.hex}"
   display_name = "Service Account for Cloud Run"
 }
@@ -71,14 +54,12 @@ resource "google_project_service" "wif_api" {
   disable_on_destroy = false
 }
 
-
 resource "google_service_account_iam_member" "workload_identity_user" {
   service_account_id = google_service_account.github_actions.name
   role               = "roles/iam.workloadIdentityUser"
   member             = "principalSet://iam.googleapis.com/projects/1054015443281/locations/global/workloadIdentityPools/gemini-rag/attribute.repository/backstage-dummy-org/GenAI-RAG-Template"
 }
 
-# # Applies permissions to the Cloud Run SA
 resource "google_project_iam_member" "allrun" {
   for_each = toset([
     "roles/cloudsql.instanceUser",
@@ -88,16 +69,15 @@ resource "google_project_iam_member" "allrun" {
     "roles/iam.serviceAccountTokenCreator",
   ])
 
-  project = module.project-services.project_id
+  project = local.project_id
   role    = each.key
   member  = "serviceAccount:${google_service_account.runsa.email}"
 }
 
-# Deploys a service to be used for the database
 resource "google_cloud_run_v2_service" "retrieval_service" {
   name     = "retrieval-service-${random_id.id.hex}"
   location = var.region
-  project  = module.project-services.project_id
+  project  = local.project_id
 
   template {
     service_account = google_service_account.runsa.email
@@ -130,7 +110,7 @@ resource "google_cloud_run_v2_service" "retrieval_service" {
       }
       env {
         name  = "DB_PROJECT"
-        value = module.project-services.project_id
+        value = local.project_id
       }
       env {
         name  = "DB_REGION"
@@ -158,15 +138,13 @@ resource "google_cloud_run_v2_service" "retrieval_service" {
         }
       }
     }
-
   }
 }
 
-# Deploys a service to be used for the frontend
 resource "google_cloud_run_v2_service" "frontend_service" {
   name     = "frontend-service-${random_id.id.hex}"
   location = var.region
-  project  = module.project-services.project_id
+  project  = local.project_id
 
   template {
     service_account = google_service_account.runsa.email
@@ -194,7 +172,6 @@ resource "google_cloud_run_v2_service" "frontend_service" {
   }
 }
 
-# # Set the frontend service to allow all users
 resource "google_cloud_run_service_iam_member" "noauth_frontend" {
   location = google_cloud_run_v2_service.frontend_service.location
   project  = google_cloud_run_v2_service.frontend_service.project
@@ -203,22 +180,17 @@ resource "google_cloud_run_service_iam_member" "noauth_frontend" {
   member   = "allUsers"
 }
 
-
-
 data "google_service_account_id_token" "oidc" {
   target_audience = google_cloud_run_v2_service.retrieval_service.uri
 }
 
-# # Trigger the database init step from the retrieval service
-# # Manual Run: curl -H "Authorization: Bearer $(gcloud auth print-identity-token)" {run_service}/data/import
-
-# tflint-ignore: terraform_unused_declarations
 data "http" "database_init" {
   url    = "${google_cloud_run_v2_service.retrieval_service.uri}/data/import"
   method = "GET"
   request_headers = {
     Accept = "application/json"
-  Authorization = "Bearer ${data.google_service_account_id_token.oidc.id_token}" }
+    Authorization = "Bearer ${data.google_service_account_id_token.oidc.id_token}"
+  }
 
   depends_on = [
     google_sql_database.database,
