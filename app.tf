4d8c03a17dd5ec3ff8ccae3923a0ee1c5981e330
Wojciech Przybylski <146734702+WojciechP-ONS@users.noreply.github.com>
2024-06-20 12:26:07 +0100

    Update app.tf

--- app.tf
+++ app.tf
@@ -13,39 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-resource "google_iam_workload_identity_pool" "gemini-rag" {
-  provider                  = google
-  project                   = var.project_id
-  workload_identity_pool_id = var.pool_id
-  display_name              = var.pool_display_name
-  description               = var.pool_description
-  disabled                  = false
-}
-
-resource "google_iam_workload_identity_pool_provider" "gemini-rag" {
-  provider                           = google
-  project                            = var.project_id
-  workload_identity_pool_id          = google_iam_workload_identity_pool.gemini-rag.name
-  workload_identity_pool_provider_id = var.provider_id
-  display_name                       = var.provider_display_name
-  description                        = var.provider_description
-  attribute_condition                = var.attribute_condition
-  attribute_mapping                  = var.attribute_mapping
-  oidc {
-    allowed_audiences = var.allowed_audiences
-    issuer_uri        = var.issuer_uri
-  }
-  depends_on = [google_iam_workload_identity_pool.gemini-rag]
-}
-
-resource "google_service_account_iam_member" "wif-sa" {
-  for_each           = var.sa_mapping
-  service_account_id = each.value.sa_name
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.gemini-rag.name}/${each.value.attribute}"
-}
-
-
 # Creates the Service Account to be used by Cloud Run
 resource "google_service_account" "runsa" {
   project      = module.project-services.project_id
