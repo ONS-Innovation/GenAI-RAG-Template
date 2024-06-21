2015f91266afb5b55e223881f3e87e8a60fef0e7
WojciechP-ONS <wojciech.przybylski@ext.ons.gov.uk>
2024-06-21 16:32:52 +0100

    udpate

--- app.tf
+++ app.tf
@@ -1,13 +1,12 @@
-
-resource "google_iam_workload_identity_pool" "github_pool" {
+resource "google_iam_workload_identity_pool" "github_pool_demo" {
   provider = google-beta
   workload_identity_pool_id = "github-pool"
   display_name = "GitHub Pool"
 }
 
-resource "google_iam_workload_identity_pool_provider" "github_provider" {
+resource "google_iam_workload_identity_pool_provider" "github_provider_demo" {
   provider = google-beta
-  workload_identity_pool_id = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
+  workload_identity_pool_id = google_iam_workload_identity_pool.github_pool_demo.workload_identity_pool_id
   workload_identity_pool_provider_id = "github-provider"
   display_name = "GitHub Provider"
  
